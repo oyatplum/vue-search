2cf6db216dfb3940e148a5a4583885f10dd19e04
yeji <oyatpeach@naver.com>
2023-01-14 18:06:12 +0900

    create search result

--- vanilla/js/models/SearchModel.js
+++ vanilla/js/models/SearchModel.js
@@ -2,12 +2,12 @@ const data = [
     {
       id: 1,
       name: '[키친르쎌] 홈메이드 칠리소스 포크립 650g',
-      image: 'https://cdn.bmf.kr/_data/product/H1821/5a4ed4e8a6751cb1cc089535c000f331.jpg'
+      image: 'https://cdn.pixabay.com/photo/2015/05/04/19/39/wide-rib-752849_1280.jpg'
     },
     {
       id: 2,
       name: '[키친르쎌] 이탈리아 파티 세트 3~4인분',
-      image: 'https://cdn.bmf.kr/_data/product/H503E/300d931e3b8252ed628b6a3c2f56936b.jpg'
+      image: 'https://mblogthumb-phinf.pstatic.net/MjAyMTEyMDZfMjAg/MDAxNjM4NzU3ODM5Njc2.0PINUt2ERbuGVNCdq57Zjj98NdVZogc28bwm215i0Acg.HlNctFLrW2eEiGPMnhqKzdSK2c5w6itaF19qw90sjAog.PNG.superkitchen_kr/%25EC%258A%2588%25ED%258D%25BC%25ED%2582%25A4%25EC%25B9%259C_%25ED%2599%2588%25ED%258C%258C%25ED%258B%25B0%25EC%2584%25B8%25ED%258A%25B8_2.png?type=w800'
     }
   ]
   
